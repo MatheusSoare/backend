7678c073771aee6cae53401f7579b4a23c4d9724
Felipe Soares <felipe_soares_2@hotmail.com>
2019-05-22 01:10:02 -0300

    correção server

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors());
+
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const cors = require('cors');
+app.use(cors());
 
 io.on('connection', socket => {
     socket.on('connectRoom', box => {
