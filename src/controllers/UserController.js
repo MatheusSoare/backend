57b9d5e6b2ef6eeea3865a0442cd170697d45c99
Leonardo <leonardo_tsuji@hotmail.com>
2019-06-18 01:11:30 -0300

    updt

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,25 @@
 const User = require('../models/User');
 
+const NodeRSA = require('node-rsa');
+const key = new NodeRSA({b: 512});
+key.setOptions({
+    encryptionScheme: 'pkcs1'
+});
+
+
+
+// const decrypted = key.decrypt(encrypted, 'utf8');
+
 class UserController{
    async store(req,res) {
-    
-
-       const user = await User.create(req.body);
+       console.log(req.body);
+       
+        req.body.username = key.decrypt(req.body.username, 'base64');
+        req.body.password = key.decrypt(req.body.password, 'base64');
+        req.body.email = key.decrypt(req.body.email, 'base64');
+        const user = await User.create(req.body);
+        console.log(req);
+        
         return res.json(user);
     }
 
